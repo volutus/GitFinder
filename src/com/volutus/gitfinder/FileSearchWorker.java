05e014785e8b577b588e2279a6115dcf623fa02d
volutus <trevcat3@hotmail.com>
2020-03-15 01:18:13 -0400

    Standard formatting, remove unused variable

--- src/com/volutus/gitfinder/FileSearchWorker.java
+++ src/com/volutus/gitfinder/FileSearchWorker.java
@@ -4,61 +4,61 @@ import java.io.File;
 
 public class FileSearchWorker implements Runnable
 {
-    private boolean working = false;
+	private boolean working = false;
 
-    boolean isWorking() {
-        return working;
-    }
+	boolean isWorking()
+	{
+		return working;
+	}
 
-    public void run()
-    {
-        // This will run constantly until the main thread is done, at which it will resolve nicely.
-        while (Main.working)
-        {
-            // This logic is a joke, but it is the best way I've found to do this. You'll see a LOT of
-            // exceptions being caught here and exception handling is expensive in Java, but I can't find
-            // a better way to do this.
-            File directory = null;
-            if (Main.directoriesToSearch.size() > 0)
-            {
-                try
-                {
-                    directory = Main.directoriesToSearch.remove(0);
-                }
-                catch (Exception e)
-                {
-                    // DO NOTHING
-                }
-            }
+	public void run()
+	{
+		// This will run constantly until the main thread is done, at which it will resolve nicely.
+		while (Main.working)
+		{
+			// This logic is a joke, but it is the best way I've found to do this. You'll see a LOT of
+			// exceptions being caught here and exception handling is expensive in Java, but I can't find
+			// a better way to do this.
+			File directory = null;
+			if (Main.directoriesToSearch.size() > 0)
+			{
+				try
+				{
+					directory = Main.directoriesToSearch.remove(0);
+				}
+				catch (Exception e)
+				{
+					// DO NOTHING
+				}
+			}
 
-            if (directory != null)
-            {
-                working = true;
-                Main.directoryCount++;
+			if (directory != null)
+			{
+				working = true;
+				Main.directoryCount++;
 
-                File[] files = directory.listFiles();
+				File[] files = directory.listFiles();
 
-                if (files != null)
-                {
-                    for (File file: files)
-                    {
-                        if (file.getName().equals(".git"))
-                        {
-                            Main.discoveredRepos.add(file.toString());
-                            Main.repoCount++;
-                        }
-                        else if (file.isDirectory())
-                        {
-                            // TODO Consider this optimization. It cuts the run-time in half, but maybe there's a better way?
-                            if (!file.getName().contains("Windows"))
-                            {
-                                Main.directoriesToSearch.add(file);
-                            }
-                        }
-                    }
-                }
-            }
-            working = false;
-        }
-    }
+				if (files != null)
+				{
+					for (File file : files)
+					{
+						if (file.getName().equals(".git"))
+						{
+							Main.discoveredRepos.add(file.toString());
+						}
+						else if (file.isDirectory())
+						{
+							// TODO Consider this optimization. It cuts the run-time in half, but maybe there's a better way?
+							if (!file.getName().contains("Windows"))
+							{
+								Main.directoriesToSearch.add(file);
+							}
+						}
+					}
+				}
+			}
+			working = false;
+		}
+	}
 }
--- src/com/volutus/gitfinder/Main.java
+++ src/com/volutus/gitfinder/Main.java
@@ -11,80 +11,79 @@ import java.util.concurrent.Executors;
 
 public class Main
 {
-    static int repoCount = 0;
-    static int directoryCount = 0;
-    static List<String> discoveredRepos = new ArrayList<>();
-    static List<File> directoriesToSearch = Collections.synchronizedList(new ArrayList<>());
-    static boolean working = true;
+	static int directoryCount = 0;
+	static List<String> discoveredRepos = new ArrayList<>();
+	static List<File> directoriesToSearch = Collections.synchronizedList(new ArrayList<>());
+	static boolean working = true;
 
-    // I've found the best performance around 200 on my machine.
-    private static final int NUMBER_OF_WORKERS = 200;
+	// I've found the best performance around 200 on my machine.
+	private static final int NUMBER_OF_WORKERS = 200;
 
-    private static final int POLLING_INTERVAL_IN_MILLIS = 100;
+	private static final int POLLING_INTERVAL_IN_MILLIS = 100;
 
-    public static void main(String[] args)
-    {
-        // We want to find all .git files on this PC. To prevent issues with scanning network drives, this program
-        // will be restricted to just the origin drive for now, although adding drive letter support may be a neat
-        // feature.
-        long start = System.nanoTime();
+	public static void main(String[] args)
+	{
+		// We want to find all .git files on this PC. To prevent issues with scanning network drives, this program
+		// will be restricted to just the origin drive for now, although adding drive letter support may be a neat
+		// feature.
+		long start = System.nanoTime();
 
-        String workingDirectory = System.getProperty("user.dir");
-        Path workingPath = Paths.get(workingDirectory);
-        Path root = workingPath.getRoot();
-        directoriesToSearch.add(root.toFile());
+		String workingDirectory = System.getProperty("user.dir");
+		Path workingPath = Paths.get(workingDirectory);
+		Path root = workingPath.getRoot();
+		directoriesToSearch.add(root.toFile());
 
-        List<FileSearchWorker> workers = new ArrayList<>();
-        ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_WORKERS);
-        for (int i = 0; i<NUMBER_OF_WORKERS; i++)
-        {
-            FileSearchWorker worker = new FileSearchWorker();
-            workers.add(worker);
-            executor.execute(worker);
-        }
+		List<FileSearchWorker> workers = new ArrayList<>();
+		ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_WORKERS);
+		for (int i = 0; i < NUMBER_OF_WORKERS; i++)
+		{
+			FileSearchWorker worker = new FileSearchWorker();
+			workers.add(worker);
+			executor.execute(worker);
+		}
 
-        while (working)
-        {
-            if (directoriesToSearch.size() == 0)
-            {
-                boolean workerStillActive = false;
-                for (FileSearchWorker worker: workers)
-                {
-                    if (worker.isWorking())
-                    {
-                        workerStillActive = true;
-                        break;      // no point to keep searching
-                    }
-                }
-                working = workerStillActive;
-            }
-            else
-            {
-                try
-                {
-                    // We'll use a polling approach to keep checking our workers.
-                    Thread.sleep(POLLING_INTERVAL_IN_MILLIS);
-                }
-                catch (Exception e)
-                {
-                    // This really shouldn't ever happen
-                    e.printStackTrace();
-                }
-            }
-        }
+		while (working)
+		{
+			if (directoriesToSearch.size() == 0)
+			{
+				boolean workerStillActive = false;
+				for (FileSearchWorker worker: workers)
+				{
+					if (worker.isWorking())
+					{
+						workerStillActive = true;
+						break;      // no point to keep searching
+					}
+				}
+				working = workerStillActive;
+			}
+			else
+			{
+				try
+				{
+					// We'll use a polling approach to keep checking our workers.
+					Thread.sleep(POLLING_INTERVAL_IN_MILLIS);
+				}
+				catch (Exception e)
+				{
+					// This really shouldn't ever happen
+					e.printStackTrace();
+				}
+			}
+		}
 
-        // Terminate the threads we made
-        executor.shutdown();
+		// Terminate the threads we made
+		executor.shutdown();
 
-        long end = System.nanoTime();
-        long run = (end - start) / 1000000;
+		long end = System.nanoTime();
+		long run = (end - start) / 1000000;
 
-        // Now print the results to the console
-        System.out.println("Scanned " + directoryCount + " directories in " + run + "ms");
-        for (String repo: discoveredRepos)
-        {
-            System.out.println("Repo exists at " + repo);
-        }
-        System.out.println("Number of repos found - " + repoCount);
-    }
+		// Now print the results to the console
+		System.out.println("Scanned " + directoryCount + " directories in " + run + "ms");
+		for (String repo: discoveredRepos)
+		{
+			System.out.println("Repo exists at " + repo);
+		}
+		System.out.println("Number of repos found - " + discoveredRepos.size());
+	}
 }
